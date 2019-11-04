23f72ed042f72b565f5d53cfc6f26d2710eaf827
Dinesh Parimi <dineshp@berkeley.edu>
2019-11-04 08:00:44 +0000

    Hierarchical module flattening to resolve naming issues

--- src/test/scala/treadle/executable/ActivityFactorSpec.scala
+++ src/test/scala/treadle/executable/ActivityFactorSpec.scala
@@ -4,6 +4,7 @@ package treadle.executable
 
 import treadle._
 import firrtl.stage.FirrtlSourceAnnotation
+import firrtl.transforms.{FlattenAnnotation, Flatten}
 import org.scalatest.{FreeSpec, Matchers}
 import treadle.{
   DataStorePlugInAnnotation,
@@ -13,8 +14,20 @@ import treadle.{
 import scala.collection.mutable
 import reporters.{ReportArea, AreaAnnotation}
 import firrtl._
+import firrtl.Parser.UseInfo
+import firrtl.passes.RemoveEmpty
+import firrtl.annotations.{ModuleName, CircuitName, ComponentName, Annotation}
 
 class ActivityFactorSpec extends FreeSpec with Matchers {
+
+  def flatten(input : String, topName : String): String = {
+      val inputCirc = firrtl.Parser.parse(input)
+      val name = ModuleName(topName, CircuitName(topName)) // If this fails, bad input
+      val flatteningAnnotation = Seq[Annotation](FlattenAnnotation(name))
+      val finalState = (new LowFirrtlCompiler).compileAndEmit(CircuitState(inputCirc, ChirrtlForm, flatteningAnnotation), Seq[Transform](new Flatten))
+      val flattenedCirc = RemoveEmpty.run(Parser.parse(finalState.getEmittedCircuit.value)).serialize
+      flattenedCirc
+  }
 
   info( "this")
 
@@ -33,14 +46,26 @@ class ActivityFactorSpec extends FreeSpec with Matchers {
           |    reg r : SInt<9>, clock
           |    r <= add(a, b)
           |    c <= add(a, b)
+          |    inst adder of Add
+          |    adder.r <= r
+          |    adder.a <= a
+          |    d <= adder.d
+          |
+          |  module Add :
+          |    input r : SInt<9>
+          |    input a : SInt<8>
+          |    output d : SInt<10>
+          |
           |    d <= add(r, a)
           |""".stripMargin
 
+      val flattenedCirc = flatten(input, "PassThrough")
+      println(flattenedCirc)
       val activityFactorCollector = new ActivityFactorCollector
       val annos = Seq(
         DataStorePlugInAnnotation("ActivityFactorCollector", activityFactorCollector.getPlugin)
       )
-      val tester = TreadleTester(annos :+ FirrtlSourceAnnotation(input))
+      val tester = TreadleTester(annos :+ FirrtlSourceAnnotation(flattenedCirc))
 
       val expectedTransitionCount = Seq(0, 2, 3, 6, 7, 9, 10, 14, 15, 17)
 
